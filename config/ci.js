dad6bedf0055bfbebd22c6cc462958b2e43e65e3
Ammar Ghazali <ammar_ghazali@yahoo.de>
2019-09-10 12:16:08 +0800

    Travis config

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     "968729885240-ef0ujseu9d3p1lt0vpfeg144u199cevo.apps.googleusercontent.com",
   googleClientSecret: "wPCC6ejG-VqAwcLjab1fLbAM",
-  mongoURI: "mongodb://127.0.0.1:27017/blogster_dev",
+  mongoURI: "mongodb://127.0.0.1:27017/blogster_ci",
   cookieKey: "123123123",
   redisUrl: "redis://127.0.0.1:6379"
 };
