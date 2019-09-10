ee0765868d650215407ad4c21d72b9ccaac7af5c
Ammar Ghazali <ammar_ghazali@yahoo.de>
2019-09-10 10:56:55 +0800

    Travis config

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "968729885240-ef0ujseu9d3p1lt0vpfeg144u199cevo.apps.googleusercontent.com",
+  googleClientSecret: "wPCC6ejG-VqAwcLjab1fLbAM",
+  mongoURI: "mongodb://127.0.0.1:27017/blogster_dev", //
+  cookieKey: "123123123",
+  redisUrl: "redis://127.0.0.1:6379" //
+};
